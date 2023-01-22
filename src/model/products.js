2bb1c30d42333ec6148392239ae3aa6ab8f078b4
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-01-22 00:09:55 +0000

    Create a new function to getProduct based on a match with a given id (challenge 3)

--- src/model/products.js
+++ src/model/products.js
@@ -1,5 +1,6 @@
 const db = require("../database/db.js");
 
+// Challenge 1
 const select_products = db.prepare(/*sql*/ `
     SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order FROM products
 `);
@@ -8,6 +9,7 @@ function listProducts() {
   return select_products.all();
 }
 
+// Challenge 2
 const search_products = db.prepare(/*sql*/ `
  SELECT id, name FROM products WHERE name LIKE ?`);
 
@@ -15,4 +17,13 @@ function searchProducts(str) {
   return search_products.all(`%${str}%`);
 }
 
-module.exports = { listProducts, searchProducts };
+// Challenge 3
+const get_product = db.prepare(/*sql*/ `
+    SELECT id, name FROM products WHERE id = ? 
+`);
+
+function getProduct(id) {
+  return get_product.get(id);
+}
+
+module.exports = { listProducts, searchProducts, getProduct };
